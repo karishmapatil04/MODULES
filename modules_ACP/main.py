870faa7bb8fc85833bb8104f6ca197fa696fb3f3
karishmapatil04 <karishma04.codingal@gmail.com>
2025-08-05 21:29:02 +0530

    1 and 2 activity

--- modules_ACP/main.py
+++ modules_ACP/main.py
@@ -1,1 +1,31 @@
-print("hello buddy")
+#Print a Sentence 
+print("Welcome to the world of programming!")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("welcome to ", end= "*")
+
+
+
+#second activity- understanding variables
+# Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
