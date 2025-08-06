910aea6f1ec27df8901fc81875eb11002f85dc2e
karsihmapatil04 <karishma04.codingal@gmail.com>
2025-08-06 21:29:51 +0530

    NO CHANGE

--- modules_ACP/main.py
+++ modules_ACP/main.py
@@ -1,31 +1,11 @@
-#Print a Sentence 
-print("Welcome to the world of programming!")
-
-#Print a Number
-print(4)
-
-#Next Line after print
-print("Hello Python \n\n welcome to coding")
-
-#Pass multiple arguments for printing
-print("hello",5)
-
-#End argument of Print Statement
-print("welcome to ", end= "*")
 
 
+text = str(input("Enter a string: "))
 
-#second activity- understanding variables
-# Creating different variables
-x = 5
-y = "John"
-print(x)
-print(y)
+# Reverse String 
+# using step value as -1 to iterate in reverse
+'''revText = text[3::1] 
+text = revText'''
 
-codingal = "codingal"
-print(codingal)
-
-# User Input
-name = input("enter your name: ")
-
-print("\nHello", name, "\nwelcome to codingal")
+print("Reverse of Given String is:")
+print(text[5])
