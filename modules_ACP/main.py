269ddcc81124c7f5cb84514e44aacabc7c24c009
karsihmapatil04 <karishma04.codingal@gmail.com>
2025-08-10 00:26:07 +0530

    changes for class

--- modules_ACP/main.py
+++ modules_ACP/main.py
@@ -6,12 +6,19 @@ if(x>y):
 else:
  print("y is greater then x")"""
 
-costprice =int(input("enter the cp: "))
-sellingprice =int(input("enter the sp: "))
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
 
-if(sellingprice>costprice):
-  print("profit")
-  pt=sellingprice-costprice
-  print(pt)
-else :
-  print("No profit")
+#sum of 40 numbers
+sum=mean1*total_number
+print("The sum of 40 numbers:",sum)
+
+#correct sum of these numbers
+SUM2=sum-(wrong_number)+(correct_number)
+print("CORRECTED SUM",SUM2)
+
+#the correct mean
+mean2=SUM2/total_number
+print(mean2)
