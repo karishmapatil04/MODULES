7b631a1d7b27fd51cca3cd38d9de8ec85f148baa
karishmapatil04 <karishma04.codingal@gmail.com>
2025-09-20 23:52:10 +0530

    LESSON-27

---
+++ PYTHON/data_type.py
@@ -0,0 +1,17 @@
+'''result=2.55
+X=int(result)
+print(X)''' 
+
+'''
+print("Enter your name")
+name=input("My name:")
+print("Hello", name, "nice to meet you")
+'''
+
+str1="Hello"
+str2=" World"
+print("str1 and str2:", str1+str2)
+
+string="Welcome"
+print(string[::-1])
+print(string[2:7:1])
