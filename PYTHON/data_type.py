9fa20c8364fecd42efad191565b81c3c1c64e725
karishmapatil04 <karishma04.codingal@gmail.com>
2025-10-03 22:45:34 +0530

    changes made

--- PYTHON/data_type.py
+++ PYTHON/data_type.py
@@ -8,10 +8,13 @@ name=input("My name:")
 print("Hello", name, "nice to meet you")
 '''
 
-str1="Hello"
+'''str1="Hello"
 str2=" World"
 print("str1 and str2:", str1+str2)
 
 string="Welcome"
 print(string[::-1])
-print(string[2:7:1])
+print(string[2:7:1])'''
+
+print("ascii value of r",ord('r'))
+print("character value of 81",chr(81))
