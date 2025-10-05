369112591d7ec63c29006481584d3987758a213f
karishmapatil04 <karishma04.codingal@gmail.com>
2025-10-05 20:33:13 +0530

    changes in python op file

--- MODULES7/python_operator.py
+++ MODULES7/python_operator.py
@@ -10,3 +10,19 @@ note_3 = ((Amount%100)%50)//10
 print( "notes of 100 rupee" , note_1)
 print("notes of 50 rupee" , note_2)
 print("notes of 10 rupee" , note_3)
+
+
+#pgm for calculating total number of years,weeks and days
+print("Enter the Number of Days: ")
+num = int(input())
+
+year = int(num/365)
+week = int((num%365)/7)
+days = int((num%365)%7)
+
+print("Total Number of Year(s): ")
+print(year)
+print("Total Number of Week(s):")
+print(week)
+print("Total Number of Day(s):")
+print(days)
