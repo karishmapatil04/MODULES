98774323db24f2a95da6a91f898c40a5dec98245
karishmapatil04 <karishma04.codingal@gmail.com>
2025-10-05 19:18:08 +0530

    act_1,2,3 files added

---
+++ PYTHON/loops_with_range.py
@@ -0,0 +1,8 @@
+str1=input("Enter a string")
+str2=('')
+
+for i in str1:
+    str2=i+str2 
+
+print("display the string",str1)    
+print("reverse of the string",str2)
---
+++ PYTHON/reverse.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number to get reverse numbers"))
+
+print("number from %d to %d"%(num,1))
+
+for j in range(num,1,-1):
+    print(j)
