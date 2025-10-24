4e89da695d1dd1af54c39874609cd552c58a4ed8
karishma04 <karishma04.codingal@gmail.com>
2025-10-24 19:23:27 +0530

    love you 3000 pgm

---
+++ MODULES10/LESSON-60/love_you_3000.py
@@ -0,0 +1,10 @@
+# Love You 3000 using Mathematical Magic 2
+
+# Step 1: Start with a number
+n = 1500
+
+# Step 2: Apply a little math "magic"
+magic = (n * 2)  # multiply by 2
+
+# Step 3: Print the result in a loving way
+print("I Love You", magic)
