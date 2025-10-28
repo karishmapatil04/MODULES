9e497c88619993df3e0ffc6174321b83116ab026
karishma04 <karishma04.codingal@gmail.com>
2025-10-28 16:50:36 +0530

    changes made for classes

--- FOR_CLASSES/choice.py
+++ FOR_CLASSES/choice.py
@@ -1,26 +1,3 @@
-print("Select your ride: ")
-print("1. Bike")
-print("2. Car")
-choice = int( input("Enter your choice: ") )
-if( choice == 1 ): 
-    print( "what type of bike? " )
-    print("1.Scooty\n")
-    print("2.Scooter\n")
-    choice2=int(input("Enter you choice2: "))
-    if choice2==1: 
-        print("you have selected scooty")
-    else:
-        ("you have selected scooter")
-
-elif( choice == 2 ): #outer elif statement
-    print( "what type of car?" )
-    print("1.Sedan")
-    print("2.XUV")
-    choice3=int(input("enter your choice3: "))
-    if choice3==1: #inner if statement
-        print("you have selected sedan")
-    else:
-        print("you have selected XUV")
-
-else: 
-print("Wrong choice!")
+decimal = int(input("Enter a decimal number: "))
+binary = bin(decimal)
+print("The binary conversion of", decimal, "is", binary[2:])
