3336a2677b6c7c393ccd66a7193ed425c12fe6e2
karishma04 <karishma04.codingal@gmail.com>
2025-11-01 16:38:31 +0530

    act_1 for argu

--- PYTHON/arguments/act_1.py
+++ PYTHON/arguments/act_1.py
@@ -1,4 +1,4 @@
-num = 29
+''''num = 29
 flag = False
 
 # check for factors
@@ -10,4 +10,11 @@ for i in range(2, num):
 if flag:
     print(num, "is not a prime number")
 else:
-    print(num, "is a prime number")
+    print(num, "is a prime number")'''
+
+def total_cal(bill_amount, tip_perc):
+    total= bill_amount*(1+0.01*tip_perc)
+    total=round(total,2)
+    print("total pay",total)
+
+total_cal(150,20)    
