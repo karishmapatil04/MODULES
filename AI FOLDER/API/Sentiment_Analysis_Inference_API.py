09d11d6889a732dadfde3c0bb889257f15b827ac
karishma04 <karishma04.codingal@gmail.com>
2025-11-11 22:49:19 +0530

    CHANGES MADE FOR CLASSES 11-11-2025

--- AI FOLDER/API/Sentiment_Analysis_Inference_API.py
+++ AI FOLDER/API/Sentiment_Analysis_Inference_API.py
@@ -37,7 +37,7 @@ for r in result:
 import requests
 
 def classify_text(text):
-    HF_API_KEY = "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"
+    HF_API_KEY = "API_KEY_HERE"
     # Correct endpoint (no router)
     API_URL = "https://router.huggingface.co/hf-inference/models/distilbert/distilbert-base-uncased-finetuned-sst-2-english"
     
