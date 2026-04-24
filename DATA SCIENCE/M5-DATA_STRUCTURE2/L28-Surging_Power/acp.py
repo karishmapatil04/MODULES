9b09751e827d4df2c7df2afab329270350a6d60c
karishmapatil04 <karishma04.codingal@gmail.com>
2026-04-24 18:04:10 +0530

    ACP

---
+++ DATA SCIENCE/M5-DATA_STRUCTURE2/L28-Surging_Power/acp.py
@@ -0,0 +1,18 @@
+def is_power_of_8(n):
+    if n <= 0:
+        return False
+    
+    while n % 8 == 0:
+        n = n // 8
+    
+    return n == 1
+
+
+# Input
+num = int(input("Enter a number: "))
+
+# Output
+if is_power_of_8(num):
+    print("Yes, it is a power of 8")
+else:
+    print("No, it is not a power of 8")
